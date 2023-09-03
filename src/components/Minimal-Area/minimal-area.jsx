d236951ae061b18c266d450c898722a68a3045e8
Ubaid-Ur-Rehman <ubaidurrehman128@gmail.com>
2023-09-03 17:38:14 +0500

    About page Video added

--- src/components/Minimal-Area/minimal-area.jsx
+++ src/components/Minimal-Area/minimal-area.jsx
@@ -16,11 +16,7 @@ const MinimalArea = () => {
         <div className="row">
           <div className="col-lg-6">
             <div className="img">
-              <img
-                className="thumparallax-down"
-                src="/img/about-us/about-section.webp"
-                alt=""
-              />
+              <img className="thumparallax-down" src="/img/about-us/about-section.webp" alt="about-us | AA Growth Grid" />
             </div>
           </div>
           <div className="col-lg-6 valign">
@@ -34,7 +30,7 @@ const MinimalArea = () => {
                   <h5> <span>1</span> AGILITY </h5>
                   {/* <img className="custom-image-class" src="/img/about-us/AGILITY.svg" /> */}
                   <p>
-                  AA Growth Grid is synonymous with agility. It's how we quickly adapt to market changes, internally and externally, to ensure products are always ideally positioned and our customers are provided with what they need. We have our fingers on every pulse in the supply and distribution, knowledge, and market chain. With rapid response and flexibility to our partner's and customer's needs, working with us helps you maximize productivity and minimize expense with no compromise on quality.
+                  AA Growth Grid is synonymous with agility. It&apos;s how we quickly adapt to market changes, internally and externally, to ensure products are always ideally positioned and our customers are provided with what they need. We have our fingers on every pulse in the supply and distribution, knowledge, and market chain. With rapid response and flexibility to our partner&apos;s and customer&apos;s needs, working with us helps you maximize productivity and minimize expense with no compromise on quality.
                   </p>
                 </li>
                 <li className="wow knowledge fadeInUp" data-wow-delay=".6s">
@@ -46,7 +42,7 @@ const MinimalArea = () => {
                 <li className="wow dedication fadeInUp" data-wow-delay="1s">
                   <h5> <span>3</span> DEDICATION </h5>
                   <p>
-                  We're truly dedicated to the growth and success of our customers and partners. Your goals become our vision, your challenges become our problem, and your needs become our actions. Through our expertise, we know how to help you arrive at your objectives and through our dedication, we can help you meet your ambitious business objectives.
+                  We&apos;re truly dedicated to the growth and success of our customers and partners. Your goals become our vision, your challenges become our problem, and your needs become our actions. Through our expertise, we know how to help you arrive at your objectives and through our dedication, we can help you meet your ambitious business objectives.
                   </p>
                 </li>
               </ul>
