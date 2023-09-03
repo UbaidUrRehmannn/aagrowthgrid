2512b4364ccfe176f03076f4109c1a94305eb395
Ubaid-Ur-Rehman <ubaidurrehman128@gmail.com>
2023-09-03 17:49:15 +0500

    about uspage two section remaining

--- src/components/text-right-image/text-right-image.jsx
+++ src/components/text-right-image/text-right-image.jsx
@@ -48,6 +48,88 @@ const ImageRight = () => {
           </div>
         </div>
       </div>
+      <div className="container">
+        <div className="row mt-70">
+        <div className="col-lg-5 img p-0">
+            <img src={AboutUs1Date.image} alt="" />
+            {/* <div className="stauts">
+              {AboutUs1Date.stauts.map((item) => (
+                <div className="item" key={item.id}>
+                  <h4>
+                    {item.number}
+                    <span>{item.letter}</span>
+                  </h4>
+                  <h6>{item.stautsName}</h6>
+                </div>
+              ))}
+            </div> */}
+          </div>
+          <div className="col-lg-7 valign md-mb50">
+            <div className="mb-50">
+              {/* <h6 className="fw-100 text-u ls10 mb-10">
+                {AboutUs1Date.smallTitle}
+              </h6> */}
+              <h3 className="fw-600 text-u ls1 mb-30 ">
+              🌐 From Struggle to Stardom
+              </h3>
+              <div className="text">
+                {/* <Split>
+                  <p className="wow txt mb-10 words chars splitting"data-splitting>
+                    {AboutUs1Date.paragraphOne}
+                  </p>
+                </Split> */}
+                <p className="text-justify">{AboutUs1Date.paragraphTwo}</p>
+              </div>
+              {/* <Link href="/case-studies">
+                <a className="butn bord curve mt-30">
+                  <span>CASE STUDIES</span>
+                </a>
+              </Link> */}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row mt-70">
+          <div className="col-lg-7 valign md-mb50">
+            <div className="mb-50">
+              {/* <h6 className="fw-100 text-u ls10 mb-10">
+                {AboutUs1Date.smallTitle}
+              </h6> */}
+              <h3 className="fw-600 text-u ls1 mb-30 ">
+              📈 Strategizing Your Success
+              </h3>
+              <div className="text">
+                {/* <Split>
+                  <p className="wow txt mb-10 words chars splitting"data-splitting>
+                    {AboutUs1Date.paragraphOne}
+                  </p>
+                </Split> */}
+                <p className="text-justify">{AboutUs1Date.paragraphThree}</p>
+              </div>
+              {/* <Link href="/case-studies">
+                <a className="butn bord curve mt-30">
+                  <span>CASE STUDIES</span>
+                </a>
+              </Link> */}
+            </div>
+          </div>
+          <div className="col-lg-5 img p-0">
+            <img src={AboutUs1Date.image} alt="" />
+            {/* <div className="stauts">
+              {AboutUs1Date.stauts.map((item) => (
+                <div className="item" key={item.id}>
+                  <h4>
+                    {item.number}
+                    <span>{item.letter}</span>
+                  </h4>
+                  <h6>{item.stautsName}</h6>
+                </div>
+              ))}
+            </div> */}
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
